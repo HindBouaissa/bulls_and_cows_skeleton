74f3da99d1a784d5382257381051cb351cec6a2b
HindBouaissa <hind.bouaissa@efrei.net>
2020-04-13 18:48:08 +0200

    Projet complet

--- src/bulls_and_cows_lib/game_solver.hpp
+++ src/bulls_and_cows_lib/game_solver.hpp
@@ -1,18 +1,23 @@
-
 #pragma once
 
 #include "board.hpp"
 #include "game_options.hpp"
+#include "random.hpp"
 #include <vector>
 
-namespace bulls_and_cows {
+namespace bulls_and_cows
+{
 
     struct PossibleSolutions
     {
-        std::vector<Code> codes;
+        std::vector<Code> codes{};
     };
 
-    PossibleSolutions generate_all_possible_codes(const GameOptions& game_options);
+    void all_possible_code(int num, int max, const GameOptions& game_options, PossibleSolutions& var_all_possible_codes,
+                           Code codes); // fonction récursive qui genere tous les codes possibles (valide ou non)
+
+    PossibleSolutions generate_all_possible_codes(
+        const GameOptions& game_options); // fonction qui genere tous les codes possibles (valide uniquement)
 
     Code pick_random_attempt(const PossibleSolutions& possible_solutions);
 
